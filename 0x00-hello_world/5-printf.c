6b071c3b177f7abb0822a966409a6124ae9f6d11
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-03-16 18:59:49 -0700

    Running c program without error message is awesomw

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - This function use printf to print a string on the screen
+ *
+ * Return: 0 alway return if their are no errors
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
