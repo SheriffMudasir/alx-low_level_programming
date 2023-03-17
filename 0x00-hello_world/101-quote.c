195aabd5740821821cc975897ad876481ad86ae9
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-03-16 20:57:29 -0700

    Second Advance Task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<uinstd.h>
+
+/**
+ * main - This program prints the assembly code 
+ *
+ * Returm: 1 
+ */
+
+ int main(void)
+ {
+ 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", fp);
+	return (0);
+
+ }
