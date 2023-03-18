a4f5876f0a86e2f4bb58c486d2aa2c7c13af885c
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-03-17 18:23:24 -0700

    First task 0-positive_or_negative.c is done

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - This program determines if a specified random number is positive, negative or a zero
+ *
+ * Return: 0 upon successful iteration
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d" is positive, n);
+	}
+	else if (n < 0)
+	{
+		printf("%d" is negative, n);
+	}
+	else
+	{
+		printf("%d" is zero, n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This is the second task of the Alx low level programming task which encompases everything about variables_if_statements_While_and_do_while_loops
