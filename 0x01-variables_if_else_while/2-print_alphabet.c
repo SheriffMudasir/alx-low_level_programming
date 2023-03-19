94b9c27400e4a4d9fd20aea424ce2998aee1ae0b
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-03-18 18:17:36 -0700

    Task 2 done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lower case
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z';)
+	{
+	putchar (ch);
+	ch++;
+	}
+	putchar ('\n');
+	return (0);
+}
