3907192a4f2127983d9be3f0a008e398d1687739
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-04-22 02:05:52 -0700

    This is 0x10-variadic_functions task of the Alx low level programming

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+
+/**
+ * sum_them_all - This function add all it parameters
+ * @n: Paramters passed to the function.
+ * @...: A variable number of paramters to find the sum
+ *
+ * Return: If n == 0 - 0.
+ * Otherwise - the sum of all parameters.
+ */
+int sum_them_all(const unsigned int n, ...)
+	{
+		va_list list;
+		unsigned int x, sum = 0;
+
+
+		va_start(list, n);
+
+
+		for (x = 0; x < n; x++)
+			sum += va_arg(list, int);
+
+
+		va_end(list);
+
+
+		return (sum);
+	}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,6 @@
+GitHub repository: alx-low_level_programming
+Directory: 0x10-variadic_functions
+File: 0-sum_them_all.c
+File: 1-print_numbers.c
+File: 2-print_strings.c
+File: 3-print_all.c
