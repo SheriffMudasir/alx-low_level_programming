07d6fedbbeb23b59d2332e057d30e94761c447f0
SheriffMudasir <125446599+SheriffMudasir@users.noreply.github.com>
2023-06-14 04:40:54 +0100

    Attempting advance task

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,11 +3,11 @@
 #include "main.h"
 
 /**
- * free_grid - This function frees a 2 dimensional grid previously created
- * @grid: 2d grid
- * @height: height dimension of grid
- * Return: nothing
- */
+* free_grid - This function frees a 2 dimensional grid previously created
+* @grid: 2d grid
+* @height: height dimension of grid
+* Return: noth ing
+*/
 void free_grid(int **grid, int height)
 {
         int x;
