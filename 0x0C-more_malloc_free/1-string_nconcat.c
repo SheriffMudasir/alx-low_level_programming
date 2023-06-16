44b56e61c366ed3fffe29a7ba7d056278d1305c9
SheriffMudasir <125446599+SheriffMudasir@users.noreply.github.com>
2023-06-16 04:42:32 +0100

    Fixing some code for better optimization

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "main.h"
+
 /**
  * *string_nconcat - This function concatenates two strings
  * @s1: string to append to
@@ -13,38 +14,38 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 	unsigned int x = 0, y = 0, num1 = 0, num2 = 0;
 
-	while (s1 && s1[num1])
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[num1])
 		num1++;
 
-	while (s2 && s2[num2])
+	while (s2[num2])
 		num2++;
 
 	if (n < num2)
 		s = malloc(sizeof(char) * (num1 + n + 1));
 	else
 		s = malloc(sizeof(char) * (num1 + num2 + 1));
-	num2++;
 
-	if (n < num2)
-		s = malloc(sizeof(char) * (num1 + n + 1));
-	else
-		s = malloc(sizeof(char) * (num1 + num2 + 1));
-
-	if (!s)
+	if (s == NULL)
 		return (NULL);
 
-	while (x < num1)
+	while (s1[x])
 	{
 		s[x] = s1[x];
 		x++;
 	}
 
-	while (n < num2 && x < (num1 + n))
+	while (n < num2 && s2[y] && x < (num1 + n))
 		s[x++] = s2[y++];
 
-	while (n >= num2 && x < (num1 + num2))
+	while (n >= num2 && s2[y] && x < (num1 + num2))
 		s[x++] = s2[y++];
 
 	s[x] = '\0';
 	return (s);
 }
+
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -4,3 +4,4 @@ File: 0-malloc_checked.c
 File: 1-string_nconcat.c
 File: 2-calloc.c
 File: 3-array_range.c
+File: 100-realloc.c
