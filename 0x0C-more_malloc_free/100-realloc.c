b71a3627297f1d6b5c088443a17f52b4111a7c08
SheriffMudasir <125446599+SheriffMudasir@users.noreply.github.com>
2023-06-18 18:08:07 +0100

    Revisited the code and optimized

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,50 +2,25 @@
 #include "main.h"
 
 /**
- * _realloc - reallocate memory block
+ * _realloc - This function that reallocates a memory block using malloc and free
  * @ptr: pointer to the memory previously allocated
- * @old_size: previous size of allocated memory
- * @new_size: new size of memory block
- *
- * Return: pointer to the new memory
+ * @ old_size:  is the size, in bytes, of the allocated space
+ * @new_size: is the new size, in bytes of the new memory block
+ * Return: pointer to the newly allaocated memory
  */
 
-void*_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned int x;
-	char *ptr1;
-	char *ptr2;
+	void *p;
 
-	if (new_size == 0 && ptr){
+	p = malloc(new_size);
+	if (new_size > old_size)
+		p = ptr;
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0){
 		free(ptr);
 		return (NULL);
 	}
-
-	if (new_size == old_size)
-		return (ptr);
-
-	if (!ptr)
-		return (malloc(new_size));
-
-	ptr1 = malloc(new_size);
-	if(!ptr1)
-		return (NULL);
-
-	ptr2 = ptr;
-
-	if (new_size < old_size)
-	{
-		for (x = 0; x < new_size; x++)
-			ptr1[x] = ptr2[x];
-	}
-
-	if (new_size > old_size){
-		for (x = 0; x < old_size; x++)
-			ptr1[x] = ptr2[x];
-	}
-
-	free(ptr);
-	return (ptr1);
-	
-
+	return (p);
 }
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,30 +1,27 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * *array_range - This function creates an array of integers
- * @min: minimum range of values stored
- * @max: maximum range of values stored and number of elements
- *
- * Return: pointer to the new array
+ * array_range - This function creates an aray of int.
+ * @min: minimum value passed from main function.
+ * @max: maximum value passed from main function
+ * Return: pointer to a newly created array.
  */
-
 int *array_range(int min, int max)
 {
-
-	int *ptr;
-	int x, size;
+	int *array;
+	int i, j; 
+	int count = 0, k = 0;
 
 	if (min > max)
 		return (NULL);
+	for (i = min; i <= max; i++)
+		count += 1;
 
-	size = max - min + 1;
-	ptr = malloc(sizeof(int) * size);
+	array = malloc(sizeof(int) * count);
+	for (j = min; j <= max; k++, j++)
+		array[k] = j;
 
-	if (ptr == NULL)
-		return (NULL);
+	return (array);
 
-	for (x = 0; min <= max; x++)
-		ptr[x] = min++;
-
-	return (ptr);
 }
+
