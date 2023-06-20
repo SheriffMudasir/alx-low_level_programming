5c47146560c44b7fc79fe3ed52e193e047948d8f
SheriffMudasir <125446599+SheriffMudasir@users.noreply.github.com>
2023-06-20 23:20:03 +0100

    Adjusting code snippets

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,8 +3,9 @@
 
 void print_dog(struct dog *d) 
 {
-        if (d == NULL) {
-        return;
+        if (d == NULL)
+        {
+                return;
         }
         printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
         printf("Age: %f\n", d->age);
