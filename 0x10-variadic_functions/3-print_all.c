1b949db48b37c7c650966069a74c5df780b531c0
SheriffMudasir <125446599+SheriffMudasir@users.noreply.github.com>
2023-06-22 19:49:07 +0100

    This is the 0x10-variadic_functions task of the alx-low_level_programming

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,56 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - This function prints anything
+ * @format: list of arguments passed to the function
+ */
+void print_all(const char * const format, ...)
+        {
+                int x = 0;
+                char *str, *stp = "";
+
+
+                va_list list;
+
+
+                va_start(list, format);
+
+
+                if (format)
+                {
+                        while (format[x])
+                        {
+                                switch (format[x])
+                                {
+                                        case 'c':
+                                                printf("%s%c", stp, va_arg(list, int));
+                                                break;
+                                        case 'i':
+                                                printf("%s%d", stp, va_arg(list, int));
+                                                break;
+                                        case 'f':
+                                                printf("%s%f", stp, va_arg(list, double));
+                                                break;
+                                        case 's':
+                                                str = va_arg(list, char *);
+                                                if (!str)
+                                                {
+                                                        str = "(nil)";
+                                                }
+                                                printf("%s%s", stp, str);
+                                                break;
+                                        default:
+                                                x++;
+                                                continue;
+                                }
+                                stp = ", ";
+                                x++;
+                        }
+                }
+
+
+                printf("\n");
+                va_end(list);
+        }
