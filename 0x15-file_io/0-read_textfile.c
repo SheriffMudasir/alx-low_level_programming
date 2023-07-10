e1194a2bc7180c33bbbdefb1f2a72c1fcdd7c2db
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-07-10 09:36:32 -0700

    Adjusting to please Betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,43 +15,43 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        int fd;
-        char *buff;
-        ssize_t read_number;
-        ssize_t write_number;
+	int fd;
+	char *buff;
+	ssize_t read_number;
+	ssize_t write_number;
 
-        if (filename == NULL)
-                return (0);
+	if (filename == NULL)
+		return (0);
 
-        fd = open(filename, O_RDONLY);
-        if (fd == -1)
-                return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-        buff = malloc(sizeof(char) * (letters + 1));
-        if (buff == NULL)
-        {
-                close(fd);
-                return (0);
-        }
+	buff = malloc(sizeof(char) * (letters + 1));
+	if (buff == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-        read_number = read(fd, buff, letters);
-        if (read_number == -1)
-        {
-                free(buff);
-                close(fd);
-                return (0);
-        }
+	read_number = read(fd, buff, letters);
+	if (read_number == -1)
+	{
+		free(buff);
+		close(fd);
+		return (0);
+	}
 
-        write_number = write(STDOUT_FILENO, buff, read_number);
-        if (write_number == -1 || write_number != read_number)
-        {
-                free(buff);
-                close(fd);
-                return (0);
-        }
+	write_number = write(STDOUT_FILENO, buff, read_number);
+	if (write_number == -1 || write_number != read_number)
+	{
+		free(buff);
+		close(fd);
+		return (0);
+	}
 
-        free(buff);
-        close(fd);
+	free(buff);
+	close(fd);
 
-        return (write_number);
+	return (write_number);
 }
