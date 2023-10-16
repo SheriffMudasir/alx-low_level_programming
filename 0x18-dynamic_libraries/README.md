1122af67d36ffc9fd93d1aaf4404e49e57c4001d
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-10-16 22:27:46 +0100

    This is the 0x18-dynamic_li

---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,4 @@
+GitHub repository: alx-low_level_programming
+Directory: 0x18-dynamic_libraries
+File: libdynamic.so, main.h
+File: 1-create_dynamic_lib.sh
---
+++ 0x18-dynamic_libraries/abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - This function computes the absolute value
+ * of an integer
+ * @n: the int to check
+ * Return: 0 if successful
+ */
+int _abs(int n)
+{
+        if (n >= 0)
+        {
+                return (n);
+        }
+        return (-n);
+}
