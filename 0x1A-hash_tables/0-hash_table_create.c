62a338c154db7d8573e90b710d53cb289639ad89
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-11-05 15:32:27 -0800

    Cleaning

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,16 +6,15 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *table;
-        int i;
+	hash_table_t *table;
+	unsigned long int i;
 
-        table = (hash_table_t *)(malloc(sizeof(hash_table_t)));
-        table->size = size;
-        table->array = (hash_table_t **)calloc(table->size, sizeof(hash_table_t *));
-        for (i = 0; i < table->size; i++)
-        {
-                table->array[i] = NULL;
-        }
-
-        return (table);
+	table = (hash_table_t *)(malloc(sizeof(hash_table_t)));
+	table->size = size;
+	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
+	for (i = 0; i < table->size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
 }
